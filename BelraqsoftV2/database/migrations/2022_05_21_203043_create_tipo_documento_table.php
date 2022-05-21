38c4b42df018c492db2b95a26b826b57af75f74d
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-05-21 16:25:40 -0500

    creacion de migraciones de las tablas tipoDocumento y Clientes

--- BelraqsoftV2/database/migrations/2022_05_21_203043_create_tipo_documento_table.php
+++ BelraqsoftV2/database/migrations/2022_05_21_203043_create_tipo_documento_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tipo_documento', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('Abreviatura',5);
+            $table->string('TipoDocumento',25)->charset('utf8mb4');
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tipo_documento');
     }
 };
---
+++ BelraqsoftV2/database/migrations/2022_05_21_204023_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre(s)',50);
+            $table->string('Apellido(s)',50);
+            $table->unsignedBigInteger('TipoDocumento');
+            $table->string('Documento',15);
+            $table->string('Correo',100)->nullable();//correo puede ser opcional
+            $table->date('FechaNacimiento')->nullable();//fecha puede ser opcional
+            $table->string('Direccion');
+            $table->string('Telefono(s)',40);
+            $table->string('Ciudad_Municipio',50);
+            $table->boolean('Estado')->default(true);
+            $table->foreign('TipoDocumento')->references('id')->on('tipo_documento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
