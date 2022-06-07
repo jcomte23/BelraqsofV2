b5d8502348a45b26e5999523a66bf68053fdf4ff
Javier Combita Tellez <javiercombita2014@gmail.com>
2022-06-07 11:31:45 -0500

    modificacion de datos de clientes

--- BelraqsoftV2/resources/views/Proveedores/Proveedores.blade.php
+++ BelraqsoftV2/resources/views/Proveedores/Proveedores.blade.php
@@ -4,103 +4,100 @@
 
 @section('content')
 
-<nav class="navbar navbar-light">
-    <div class="container-fluid barraSuperior">
-        <h1 class="fw-bold">{{ $modulo }}</h1>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-            data-bs-target="#FormularioRegistroProveedores"><i class="bi bi-person-plus-fill"></i> Nuevo</button>
-    </div>
-</nav>
+    <nav class="navbar navbar-light">
+        <div class="container-fluid barraSuperior">
+            <h1 class="fw-bold">{{ $modulo }}</h1>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                data-bs-target="#FormularioRegistroProveedores"><i class="bi bi-person-plus-fill"></i> Nuevo</button>
+        </div>
+    </nav>
 
     <div class="modal fade" id="FormularioRegistroProveedores" tabindex="-1"
-            aria-labelledby="FormularioRegistroProveedoresLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="FormularioRegistroProveedoresLabel">Registro de
-                            {{ $modulo }}</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form class="text-center fst-italic needs-validation row formulario"
-                            method="post">
-                            <form class="text-center fst-italic needs-validation row formulario"
-                                method="POST">
-                                <!-- NIVEL 1 -->
-                                <div class="col-lg-5">
-                                    <label class="label">Ingrese nombre o razon
-                                        social:</label><br>
-                                    <input class="IngresoDatos form-control" type="text"
-                                        name="txtNombre" placeholder="" required>
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="label">Tipo:</label><br>
-                                    <select class="form-select IngresoDatos form-control">
-                                    @foreach ($Documentos as $Documento )
-                                    <option value="">{{$Documento->Abreviatura }}</option>
-                                    @endforeach
-                                    </select>
-                                </div>
-                                <div class="col-lg-4">
-                                    <label class="label">Numero / NIT:</label><br>
-                                    <input class="IngresoDatos form-control" type="text"
-                                        name="txtNumero" placeholder="" required>
-                                </div>
-
-                                    <!-- NIVEL 2 -->
-                                    <!---->
-                                    <div class="col-lg-3">
-                                        <label class="label">Telefono</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txttelefono"
-                                            placeholder="" required>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <label class="label">Direccion</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txtDireccion"
-                                            placeholder="" required>
-                                    </div>
-                                    <div class="col-lg-3">
-                                        <label class="label">Ciudad / Municipio</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txtCiudad" placeholder=""
-                                            required>
-                                    </div>
-
-                                    <!-- NIVEL 3 -->
-                                    <!---->
-                                    <div class="col-lg-8">
-                                        <label class="label">Ingrese nombre del
-                                            contacto:</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txtContacto"
-                                            placeholder="" required>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <label class="label">Ingrese numero del
-                                            contacto:</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txtNumeroContacto"
-                                            placeholder="" required>
-                                    </div>
-
-                                    <!-- NIVEL 4-->
-                                    <!---->
-                                    <div class="col-lg-12">
-                                        <label class="label">Correo:</label><br>
-                                        <input class="IngresoDatos form-control" type="text" name="txtCorreo" required>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <label class="label">Descripcion del proveedor:</label><br>
-                                        <input class="IngresoDatos form-control" style="height: 80px;" type="textarea"
-                                            name="txtDescripcion" placeholder=" ">
-                                    </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                            <input class="btn btn-success confirmar_o_cancelar" type="submit" value="Actualizar">
-                        </div>
-                        </form>
-                    </div>
+        aria-labelledby="FormularioRegistroProveedoresLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="FormularioRegistroProveedoresLabel">Registro de
+                        {{ $modulo }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <div class="modal-body">
+                    <form class="text-center fst-italic needs-validation row formulario" method="post">
+                        <form class="text-center fst-italic needs-validation row formulario" method="POST">
+                            <!-- NIVEL 1 -->
+                            <div class="col-lg-5">
+                                <label class="label">Ingrese nombre o razon
+                                    social:</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtNombre" placeholder=""
+                                    required>
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="label">Tipo:</label><br>
+                                <select class="form-select IngresoDatos form-control">
+                                    @foreach ($Documentos as $Documento)
+                                        <option value="">{{ $Documento->Abreviatura }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="label">Numero / NIT:</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtNumero" placeholder=""
+                                    required>
+                            </div>
+
+                            <!-- NIVEL 2 -->
+                            <!---->
+                            <div class="col-lg-3">
+                                <label class="label">Telefono</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txttelefono" placeholder=""
+                                    required>
+                            </div>
+                            <div class="col-lg-6">
+                                <label class="label">Direccion</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtDireccion" placeholder=""
+                                    required>
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="label">Ciudad / Municipio</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtCiudad" placeholder=""
+                                    required>
+                            </div>
+
+                            <!-- NIVEL 3 -->
+                            <!---->
+                            <div class="col-lg-8">
+                                <label class="label">Ingrese nombre del
+                                    contacto:</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtContacto" placeholder=""
+                                    required>
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="label">Ingrese numero del
+                                    contacto:</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtNumeroContacto" placeholder=""
+                                    required>
+                            </div>
+
+                            <!-- NIVEL 4-->
+                            <!---->
+                            <div class="col-lg-12">
+                                <label class="label">Correo:</label><br>
+                                <input class="IngresoDatos form-control" type="text" name="txtCorreo" required>
+                            </div>
+                            <div class="col-lg-12">
+                                <label class="label">Descripcion del proveedor:</label><br>
+                                <input class="IngresoDatos form-control" style="height: 80px;" type="textarea"
+                                    name="txtDescripcion" placeholder=" ">
+                            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <input class="btn btn-success confirmar_o_cancelar" type="submit" value="Actualizar">
+                </div>
+                </form>
             </div>
+        </div>
+    </div>
 
     <table id="myTable" class="table text-center align-middle display">
         <thead>
@@ -110,7 +107,7 @@
                 <th scope="col">Descripcion</th>
                 <th scope="col">Contacto Interno</th>
                 <th scope="col">Teléfono Contacto</th>
-                <th scope="col">Correo</th>      
+                <th scope="col">Correo</th>
                 <th scope="col">Direccion</th>
                 <th scope="col">Operaciones</th>
             </tr>
@@ -229,10 +226,10 @@
 
                     </td>
                 </tr>
-                @empty
+            @empty
                 <tr>Sin proveedores</tr>
             @endforelse
         </tbody>
-        
+
     </table>
 @endsection
