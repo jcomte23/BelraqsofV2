caf26bbe982ee9999f2ff6b9cd953124363ade06
Sebastian <sebas4523velasquez@gmail.com>
2022-06-15 17:13:27 +0200

    Cambio login

--- BelraqsoftV2/routes/web.php
+++ BelraqsoftV2/routes/web.php
@@ -23,35 +23,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [LoginsController::class,"index"])->name('loginIndex');
-Route::get('Reportes',[ReportesController::class,"index"])->name('reporteIndex');
-Route::get('Clientes',[ClientesController::class,"index"])->name('clienteIndex');
-Route::post('Clientes',[ClientesController::class,"create"])->name('clienteRegistrar');
-Route::put('Clientes/{cliente?}',[ClientesController::class,"actualizar"])->name('clienteActualizar');
-Route::delete('Clientes/{cliente?}',[ClientesController::class,"eliminar"])->name('clienteEliminar');
+Route::get('/', [LoginsController::class, "index"])->name('loginIndex');
+Route::get('Reportes', [ReportesController::class, "index"])->name('reporteIndex');
+Route::get('Clientes', [ClientesController::class, "index"])->name('clienteIndex');
+Route::post('Clientes', [ClientesController::class, "create"])->name('clienteRegistrar');
+Route::put('Clientes/{cliente?}', [ClientesController::class, "actualizar"])->name('clienteActualizar');
+Route::delete('Clientes/{cliente?}', [ClientesController::class, "eliminar"])->name('clienteEliminar');
 // Route::get('Clientes/{idCliente}',[ClientesController::class,"view"])->name('viewCliente');
 
-Route::get('Usuarios',[UsuariosController::class,"index"])->name('usuarioIndex');
+Route::get('usuario', [UsuariosController::class, "index"])->name('usuarioIndex');
+Route::get('users', [UsuariosController::class, "index"])->name('users');
+// Route::get('Usuario',[UsuariosController::class,"index"])->name('clienteIndex');
 
-Route::get('Compras',[ComprasController::class,"index"])->name('compraIndex');
-Route::get('Compras/Registrar',[ComprasController::class,"registrar"])->name('compraRegistrar');
-Route::get('Compras/Editar',[ComprasController::class,"editar"])->name('compraEditar');
+Route::get('Compras', [ComprasController::class, "index"])->name('compraIndex');
+Route::get('Compras/Registrar', [ComprasController::class, "registrar"])->name('compraRegistrar');
+Route::get('Compras/Editar', [ComprasController::class, "editar"])->name('compraEditar');
 
-Route::get('Pedidos',[PedidosController::class,"index"])->name('pedidoIndex');
-Route::get('Pedidos/Registrar',[PedidosController::class,"registrar"])->name('pedidoRegistrar');
-Route::get('Pedidos/Editar',[PedidosController::class,"editar"])->name('pedidoEditar');
+Route::get('Pedidos', [PedidosController::class, "index"])->name('pedidoIndex');
+Route::get('Pedidos/Registrar', [PedidosController::class, "registrar"])->name('pedidoRegistrar');
+Route::get('Pedidos/Editar', [PedidosController::class, "editar"])->name('pedidoEditar');
 
-Route::get('Ventas',[VentasController::class,"index"])->name('ventaIndex');
-Route::get('Proveedores',[ProveedoresController::class,"index"])->name('proveedorIndex');
-Route::get('Existencias',[ExistenciasController::class,"index"])->name('existenciaIndex');
+Route::get('Ventas', [VentasController::class, "index"])->name('ventaIndex');
+Route::get('Proveedores', [ProveedoresController::class, "index"])->name('proveedorIndex');
+Route::get('Existencias', [ExistenciasController::class, "index"])->name('existenciaIndex');
 
- Route::get('{modulo}/{opcion?}',[generalController::class,"index"]);
- Route::middleware([
-     'auth:sanctum',
-     config('jetstream.auth_session'),
-     'verified'
- ])->group(function () {
-     Route::get('/dashboard', function () {
-         return view('dashboard');
-     })->name('dashboard');
+Route::get('dashboard', [LoginsController::class, "dashboard"])->name('dashboard.dashboard');
+
+
+//Route::get('{modulo}/{opcion?}', [generalController::class, "index"]);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    //  Route::get('/dashboard', function () {
+    //      return view('dashboard');
+    //  })->name('dashboard');
+    Route::get('dashboard', [ReportesController::class, "index"])->name('reporteIndex');
 });
